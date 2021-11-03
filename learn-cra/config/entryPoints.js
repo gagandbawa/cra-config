6237210a35c719fb56d74a4e19f0eb826bd67560
Gagan Bawa <gagan.d.bawa@gmail.com>
2021-11-03 09:37:57 -0700

    fix: added entry points and updated paths

---
+++ learn-cra/config/entryPoints.js
@@ -0,0 +1,10 @@
+const entryPoints = [
+	{
+		name: 'aboutme',
+		title: 'About Me',
+		appHtml: 'templates/index.html',
+		appIndexJs: 'src/routes/AboutMe/index',
+		appURL: 'aboutme.html',
+	},
+];
+module.exports = entryPoints;
